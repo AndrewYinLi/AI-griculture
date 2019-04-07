a548feea90789a4835f762dd1bb9f0db0fd2f8bb
Andrew Yin Li <ali@oberlin.edu>
2019-04-07 06:30:06 -0700

    Add files via upload

--- dev.py
+++ dev.py
@@ -14,8 +14,8 @@ microcontrollerIP = "http://172.31.32.74:3000/"
 random_forest = RandomForestClassifier(n_estimators = 200, n_jobs = -1);
 mem = np.genfromtxt("sensor_baseline.csv", delimiter=",").tolist()
 cache = []
-moistureShortPath = "moistureshort.png"
-moistureLongPath = "moisturelong.png"
+moistureShortPath = "moistureshort_test.png"
+moistureLongPath = "moisturelong_test.png"
 memGlobal = None
 cacheGlobal = None
 
@@ -51,28 +51,31 @@ def predict_json(project, model, instances, version=None):
 	return response['predictions']
 
 def update(cache):
-	global memGlobal
-	global cacheGlobal
-	if len(cache) > 0:
-		aggregate = np.array(mem + cache).astype(float).astype(int)
-		random_forest.fit(aggregate[:, 1:], aggregate[:, 0])
-	else:
-		memNp = np.array(mem)
-		random_forest.fit(memNp[:, 1:], memNp[:, 0])
-	sensor_data = requests.get(microcontrollerIP)._content.decode("utf-8").split(",")
-	#prediction = random_forest.predict([sensor_data]);
-	prediction = predict_json("plant-controller", "random_forest",[sensor_data], "random_forest_v1")
-	if len(cache) > 1000:
-		del cache[0]
-	cache.append([prediction[0]] + sensor_data)
-	light = 0
-	currentDT = datetime.datetime.now()
-	if (currentDT.hour >= 6 and currentDT.hour < 9) or int(sensor_data[0]) < 50:
-		light = 1
-	requests.post(microcontrollerIP, data = {"pump":int(prediction[0]), "light":light})
+	memNp = np.array(mem)
+	random_forest.fit(memNp[:, 1:], memNp[:, 0])
+	pred = predict_json("plant-controller", "random_forest",[[46,590,14,25]], "random_forest_v1")
+	print(pred)
+	# global memGlobal
+	# global cacheGlobal
+	# if len(cache) > 0:
+	# 	aggregate = np.array(mem + cache).astype(float).astype(int)
+	# 	random_forest.fit(aggregate[:, 1:], aggregate[:, 0])
+	# else:
+	# 	memNp = np.array(mem)
+	# 	random_forest.fit(memNp[:, 1:], memNp[:, 0])
+	# sensor_data = requests.get(microcontrollerIP)._content.decode("utf-8").split(",")
+	# prediction = random_forest.predict([sensor_data]);
+	# if len(cache) > 1000:
+	# 	del cache[0]
+	# cache.append([prediction[0]] + sensor_data)
+	# light = 0
+	# currentDT = datetime.datetime.now()
+	# if (currentDT.hour >= 6 and currentDT.hour < 9) or int(sensor_data[0]) < 50:
+	# 	light = 1
+	# requests.post(microcontrollerIP, data = {"pump":int(prediction[0]), "light":light})
 	
-	memGlobal = mem
-	cacheGlobal = cache
+	# memGlobal = mem
+	# cacheGlobal = cache
 
 	threading.Timer(2.0, update, [cache]).start()
 
@@ -81,10 +84,20 @@ update(cache)
 
 @app.route('/')
 def index():
-	memNp = np.array(memGlobal)
-	cacheNp = np.array(cacheGlobal)
-	plt.title("Short-term Moisture")
-	plt.plot(np.array(range(1, cacheNp[:, 2].shape[0]+1)), cacheNp[:, 2], '-', color="g")
+	# memNp = np.array(memGlobal)
+	# cacheNp = np.array(cacheGlobal)
+	# plt.title("Short-term Moisture")
+	# plt.plot(np.array(range(1, cacheNp[:, 2].shape[0]+1)), cacheNp[:, 2], '-', color="g")
+	# plt.xlabel("Seconds")
+	# plt.ylabel("Analog Moisture Level")
+	# if os.path.exists(moistureShortPath):
+	# 	os.remove(moistureShortPath)
+	# plt.savefig(moistureShortPath)
+	# plt.clf()
+
+	plt.title("Long-term Moisture")
+	plt.plot(np.array(range(1, memNp[:, 2].shape[0]+1)), memNp[:, 2], '-', color="r")
+	
 	plt.xlabel("Seconds")
 	plt.ylabel("Analog Moisture Level")
 	if os.path.exists(moistureShortPath):
@@ -94,8 +107,7 @@ def index():
 
 	plt.title("Long-term Moisture")
 	plt.plot(np.array(range(1, memNp[:, 2].shape[0]+1)), memNp[:, 2], '-', color="r")
-	if(cacheNp.shape[0] > 0):
-		plt.plot(np.array(range(memNp[:, 2].shape[0]+1, memNp[:, 2].shape[0]+1 + cacheNp[:,2].shape[0])), cacheNp[:, 2], '-', color="g")
+	
 	plt.xlabel("Seconds")
 	plt.ylabel("Analog Moisture Level")
 	if os.path.exists(moistureLongPath):
